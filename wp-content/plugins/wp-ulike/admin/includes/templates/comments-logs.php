3e69735302210b9d992f70dd140ee808d7c405e0
John Walker <kelvin@fourmilab.ch>
2020-02-21 23:12:30 +0000

    Updated the WP ULike plug-in to version 4.1.6.

--- wp-content/plugins/wp-ulike/admin/includes/templates/comments-logs.php
+++ wp-content/plugins/wp-ulike/admin/includes/templates/comments-logs.php
@@ -36,6 +36,28 @@
 	}
 ?>
 <div class="wrap wp-ulike-container">
+<div class="wp-ulike-pro-stats-banner wp-ulike-row">
+		<div class="col-12">
+			<div class="wp-ulike-inner">
+				<div class="wp-ulike-row">
+					<h3><?php echo _e( 'Check Votings, Best Likers & Top contents', WP_ULIKE_SLUG ); ?></h3>
+					<?php
+						echo sprintf( '<p>%s</p><div class="wp-ulike-button-group">%s%s</div>', __('With WP ULike Pro comprehensive Statistics tools, you can track what your users love and what annoys them in an instance. You can extract reports of likes and dislikes in Linear Charts, Pie Charts or whichever you prefer with dateRange picker and status selector controllers, no confusing options and coding needed.',WP_ULIKE_SLUG), wp_ulike_widget_button_callback( array(
+							'label'         => __( 'Buy WP ULike Premium', WP_ULIKE_SLUG ),
+							'color_name'    => 'default',
+							'link'          => WP_ULIKE_PLUGIN_URI . 'pricing/?utm_source=statistics-page&utm_campaign=gopro&utm_medium=wp-dash',
+							'target'        => '_blank'
+						) ), wp_ulike_widget_button_callback( array(
+							'label'         => __( 'More information', WP_ULIKE_SLUG ),
+							'color_name'    => 'info',
+							'link'          => WP_ULIKE_PLUGIN_URI . '?utm_source=statistics-page&utm_campaign=gopro&utm_medium=wp-dash',
+							'target'        => '_blank'
+						) ) );
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="wp-ulike-row">
 		<div class="col-12">
 			<h2 class="wp-ulike-page-title"><?php _e('WP ULike Logs', WP_ULIKE_SLUG); ?></h2>
@@ -62,8 +84,12 @@
 				</thead>
 				<tbody class="wp_ulike_logs">
 					<?php
-					foreach ( $datasets['data_rows'] as $data_row )
-					{
+					foreach ( $datasets['data_rows'] as $data_row ) {
+					$comment_author = $comment_content = __('Not Found!',WP_ULIKE_SLUG);
+					if( NULL != ( $getComment = get_comment( $data_row->comment_id ) ) ){
+						$comment_author  = $getComment->comment_author;
+						$comment_content = $getComment->comment_content;
+					}
 					?>
 					<tr <?php if ($alternate == true) echo 'class="alternate"';?>>
 						<td>
@@ -93,12 +119,12 @@
 						</td>
 						<td>
 						<?php
-							echo get_comment_author( ! empty( $data_row->comment_id ) ? $data_row->comment_id : 0 );
+							echo $comment_author;
 						?>
 						</td>
 						<td>
 						<?php
-							echo get_comment_text( ! empty( $data_row->comment_id ) ? $data_row->comment_id : 0 );
+							echo $comment_content;
 						?>
 						</td>
 						<td>
